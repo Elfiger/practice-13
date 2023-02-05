a8c2b49611f51e5c9037b087a189cf35eb01d441
Elfiger <68121159+Elfiger@users.noreply.github.com>
2023-02-05 12:01:12 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+﻿function calc(a, b, operation){
+    switch(operation) {
+        case 'add':
+            return a+b;
+            break;
+        case 'multi':
+            return a*b;
+            break;
+        case 'subtract':
+            return a-b;
+            break;
+        default:
+            return 'no operation';             
+    }
+}
+
+console.log(calc(1,2,'add'));
+console.log(calc(1,2,'multi'));
+console.log(calc(3,2,'subtract'));
